bb2e043860e798806457ca5f055ee33a3301190e
Andrés Miguel Guzmán Cruz <guzmancruzandresmiguel@gmail.com>
2024-09-04 01:14:06 -0600

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   ],
   icons: [
     { rel: "apple-touch-icon", url: "/icon-128.png" },
-    { rel: "icon", url: "/icon-128.png" },
+    { rel: "icon", type: "image/x-icon", url: "/favicon.ico" },  // Asegura que esté bien especificado
   ],
 };
 
@@ -49,6 +49,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, shrink-to-fit=no, viewport-fit=cover" />
         <meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#000" media="(prefers-color-scheme: dark)" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />  {/* Asegúrate de incluir el favicon aquí */}
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </head>
       <body>{children}</body>
