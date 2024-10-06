2aa46e5953024083505b6c0c8057d50c350ab05a
Sebastian <isebase24@gmail.com>
2024-10-05 23:03:30 -0600

    Add Line, Station, and Transport interfaces

---
+++ src/types/line.ts
@@ -0,0 +1,8 @@
+export interface Line {
+    id: number;
+    name: string;
+    transport: string;
+    incident: string;
+    speed: number;
+    information: string;
+}
---
+++ src/types/station.ts
@@ -0,0 +1,8 @@
+export interface Station {
+    id: number;
+    name: string;
+    line: number;
+    incident: string;
+    services: string;
+    information: string;
+}
