9409bd8ce818ca399b557933d705bd2dafc25c34
Sebastian <isebase24@gmail.com>
2024-11-05 12:21:40 -0600

    Add supervisor edit page and component with data fetching and form handling

---
+++ src/app/admin/supervisors/[id]/edit.tsx
@@ -0,0 +1,14 @@
+// pages/admin/supervisors/[id]/edit.tsx
+
+import AdminSupervisorEdit from '@/components/admin/AdminSupervisorEdit';
+import Layout from '@/components/Layout';
+
+const EditSupervisorPage = () => {
+  return (
+    <Layout>
+      <AdminSupervisorEdit />
+    </Layout>
+  );
+};
+
+export default EditSupervisorPage;
