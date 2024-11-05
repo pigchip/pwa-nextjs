b8db42a92b2247425d84aa417df6ecc89d9ea0fa
Sebastian <isebase24@gmail.com>
2024-11-05 11:40:07 -0600

    Add API route for fetching supervisors with error handling

---
+++ src/app/api/supervisors/route.ts
@@ -0,0 +1,61 @@
+// pages/api/supervisors/route.ts
+import { Supervisor } from '@/types/supervisor';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    const supervisors: Supervisor[] = await getSupervisors();
+    return NextResponse.json(supervisors, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    if (error instanceof FetchError) {
+      return NextResponse.json({
+        timestamp: new Date().toISOString(),
+        status: error.status,
+        error: error.message,
+        path: req.url,
+      }, { status: error.status });
+    }
+    return NextResponse.json({
+      timestamp: new Date().toISOString(),
+      status: 500,
+      error: 'Internal Server Error',
+      message: 'An unexpected error occurred',
+      path: req.url,
+    }, { status: 500 });
+  }
+}
+
+async function getSupervisors(): Promise<Supervisor[]> {
+  const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+  if (!apiUrl) {
+    throw new Error('API base URL is not defined');
+  }
+
+  const response = await fetch(`${apiUrl}/api/supervisors`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    console.log(errorData);
+    throw new FetchError(response.status, errorData.message || 'Failed to fetch supervisors');
+  }
+
+  const data: Supervisor[] = await response.json();
+
+  return data; // Return the raw data array
+}
+
+class FetchError extends Error {
+  status: number;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+  }
+}
