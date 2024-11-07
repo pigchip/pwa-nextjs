2d010508ad829549c074d3b71021b04320d19aa5
Sebastian <isebase24@gmail.com>
2024-11-06 22:40:32 -0600

    Enhance map icons by adjusting sizes and adding background colors

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -14,12 +14,20 @@ export const createStartIcon = () => {
       height: 50px;
       border-radius: 50%;
       border: 2px solid #00bcd4;
+      background-color: rgba(0, 188, 212, 0.2);
       position: absolute;
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
       z-index: 1;
-    "></div>
+    ">
+      <div style="
+        width: 20px;
+        height: 20px;
+        background-color: #00bcd4;
+        border-radius: 50%;
+      "></div>
+    </div>
   `;
 
   return L.divIcon({
@@ -38,25 +46,32 @@ export const createEndIcon = () => {
       align-items: center;
       justify-content: center;
       position: relative;
-      width: 30px;
-      height: 30px;
+      width: 50px;
+      height: 50px;
       border-radius: 50%;
-      background-color: #ff5722;
       border: 2px solid #ff5722;
+      background-color: rgba(255, 87, 34, 0.2);
       position: absolute;
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
       z-index: 1;
-    "></div>
+    ">
+      <div style="
+        width: 20px;
+        height: 20px;
+        background-color: #ff5722;
+        border-radius: 50%;
+      "></div>
+    </div>
   `;
 
   return L.divIcon({
     html: iconHTML,
     className: '',
-    iconSize: [30, 30],
-    iconAnchor: [15, 15],
-    popupAnchor: [0, -15],
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+    popupAnchor: [0, -25],
   });
 };
 
