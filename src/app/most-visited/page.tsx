c0dec8de5c5cae837184995116fea7b5ed2b860d
Sebastian <isebase24@gmail.com>
2024-11-10 17:21:32 -0600

    Add MostVisitedPage component to render MostVisited

---
+++ src/app/most-visited/page.tsx
@@ -0,0 +1,10 @@
+// FILE: app/most-visited/page.tsx
+'use client';
+
+import MostVisited from '@/components/MostVisited';
+
+const MostVisitedPage: React.FC = () => {
+  return <MostVisited />;
+};
+
+export default MostVisitedPage;
