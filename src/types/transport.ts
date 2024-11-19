3c78762dda2d932180a1eb6c43db2653f0772180
Sebastian <isebase24@gmail.com>
2024-11-19 16:38:05 -0600

    Enhance Transport types with DetailedTransport interface and improve formatting

--- src/types/transport.ts
+++ src/types/transport.ts
@@ -1,17 +1,24 @@
 export interface Transport {
-    name: TransportName;
-    speed: number;
+  name: TransportName;
+  speed: number;
 }
 
 export type TransportName =
-  | 'Sistema de Transporte Colectivo Metro'
-  | 'Metrobús'
-  | 'Red de Transporte de Pasajeros, Servicio Ordinario'
-  | 'Red de Transporte de Pasajeros, Servicio Expreso'
-  | 'Red de Transporte de Pasajeros, Servicio Ecobús'
-  | 'Red de Transporte de Pasajeros, Servicio Nochebús'
-  | 'Trolebús'
-  | 'Trolebús Elevado'
-  | 'Nochebús'
-  | 'Tren Ligero'
-  | 'Cablebús';
+  | "Sistema de Transporte Colectivo Metro"
+  | "Metrobús"
+  | "Red de Transporte de Pasajeros, Servicio Ordinario"
+  | "Red de Transporte de Pasajeros, Servicio Expreso"
+  | "Red de Transporte de Pasajeros, Servicio Ecobús"
+  | "Red de Transporte de Pasajeros, Servicio Nochebús"
+  | "Trolebús"
+  | "Trolebús Elevado"
+  | "Nochebús"
+  | "Tren Ligero"
+  | "Cablebús";
+
+export interface DetailedTransport extends Transport {
+  id: number;
+  transport: TransportName;
+  incident: string;
+  information: string;
+}
