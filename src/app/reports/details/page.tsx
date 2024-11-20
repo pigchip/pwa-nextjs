894f9c012b61d312454ac3f9e37bcd720d7aaf40
Sebastian <isebase24@gmail.com>
2024-11-19 20:35:35 -0600

    Refactor EvidenceDetails component to use ConfirmModal for state change confirmation

--- src/app/reports/details/page.tsx
+++ src/app/reports/details/page.tsx
@@ -7,10 +7,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useRole } from "@/contexts/RoleContext";
 import { useState } from "react";
 import { Status } from "@/types/register";
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import ConfirmModal from "@/components/ConfirmModal";
 
 const EvidenceDetails: React.FC = () => {
   const { selectedReport, setSelectedReport, updateReport } = useReports();
@@ -168,29 +165,11 @@ const EvidenceDetails: React.FC = () => {
         </div>
       </div>
 
-      <Modal
+      <ConfirmModal
         open={isModalOpen}
         onClose={handleModalClose}
-        aria-labelledby="modal-title"
-        aria-describedby="modal-description"
-      >
-        <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded shadow-lg">
-          <Typography id="modal-title" variant="h6" component="h2">
-            Confirmar Cambio de Estado
-          </Typography>
-          <Typography id="modal-description" sx={{ mt: 2 }}>
-            Estás seguro que deseas validar este incidente? La estación se verá afectada.
-          </Typography>
-          <div className="mt-4 flex justify-end">
-            <Button onClick={handleModalClose} className="mr-2" style={{ backgroundColor: '#6ABDA6', color: 'white' }}>
-              Cancelar
-            </Button>
-            <Button onClick={handleModalConfirm} variant="contained" style={{ backgroundColor: '#6ABDA6', color: 'white' }}>
-              Confirmar
-            </Button>
-          </div>
-        </Box>
-      </Modal>
+        onConfirm={handleModalConfirm}
+      />
     </Layout>
   );
 };
